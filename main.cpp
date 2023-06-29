f3bca46fa3de90ecf9ccda70db09f0d523944ae9
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-06-29 09:33:57 -0300

    tentando resolver problema da matriz

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 
 using namespace std;
 
+//variaveis globais
+    const int maxli=100, maxcol=2;
+    int op, linha=0, coluna=1;
+    int nlinha=1, ncoluna=2;
+    string contato[maxli][maxcol];
+    string nome[50], telefone[50];
+
 //declarando funcoes
-void cadastrar(string x[][2]);
+void cadastrar(string x[maxli][maxcol],int tam_linha, int tam_coluna);
 void listar();
+void procurar(string y[][2]);
 //
 
-    int op, linha=0, coluna=1;
-    string contato[100][2];
 
 int main (){
     setlocale(LC_ALL, "portuguese");
 
-    bool opi= true;
     fstream arquivo;
     arquivo.open("Agenda2023.txt", ios::out | ios::app);
     if (arquivo.is_open()){
@@ -35,16 +40,40 @@ int main (){
 
         switch(op){
             case 1:
-                 cadastrar(contato);
+          
+                while(nlinha <100){
+                        cadastrar(contato, nlinha, ncoluna);
+                        break;
+                        nlinha++;
+                        cout << nlinha;
+                }
+                
+                break;
+            case 2:
+                //listar();//
+                cout << endl;
+
+            for (linha = 0; linha < maxli; linha++)
+            {
+           
+            for (coluna  = 0; coluna < maxcol; coluna++)
+            {
+                     cout<<contato[2][coluna];
+            }
+            }
                 break;
 
-            case 2:
-                listar();
+            case 8:
+                cout << "Saindo...";
+                break;
+
+            dafault:
+                cout << "Opção inválida";
                 break;
             }
 
     
-        }while (op!=7);
+        }while (op!=8);
     }else{
         cout << "Não foi possivel abrir o arquivo"<<endl;
     }
@@ -54,29 +83,35 @@ int main (){
     return 0;
 }
 
-void cadastrar(string x[][2]){
+void cadastrar(string x[maxli][maxcol], int tam_linha, int tam_coluna){
 
-    int linhaprox=0;
+   
 
     fstream arquivo;
     arquivo.open("Agenda2023.txt", ios::out | ios::app);
     
-            
-        cout << "Digite seu nome:"<< endl;
-        for (linha = 0; linha < 1; linha++)
+    
+
+        cout<< "Digite seu nome:"<<endl;
+        
+        for (linha = 0; linha < tam_linha; linha++)
         {
             cout << "Digite seu numero:"<<endl;
-            for (coluna  = 0; coluna < 2; coluna++)
+            for (coluna  = 0; coluna < tam_coluna; coluna++)
             {
             cin>>x[linha][coluna];
-            arquivo << x[linha][coluna];
-            arquivo << " ";
+
+            arquivo << x[linha][coluna]<<" ";
             arquivo << endl;
+            if(tam_linha > tam_linha+1){
+                break;
+            }
                
             }
             
             
-        }
+            cout << endl;
+        }      
         
         
     arquivo.close();
@@ -88,13 +123,20 @@ void cadastrar(string x[][2]){
 //função de listar 
 void listar (){
 
-    string linha;
+    string line;
     fstream arquivo2;
     arquivo2.open("Agenda2023.txt", ios:: in);
-    while (getline(arquivo2, linha))
+    while (getline(arquivo2, line))
     {
-        cout << linha<< " ";
+        cout << line<< " ";
         cout << endl;
     }
     
 }
+
+//funcao de procurar
+
+
+
+
+
