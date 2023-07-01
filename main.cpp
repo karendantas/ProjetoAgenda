65be3f0b453b41fbf7de82d7d6c4b4e42eeb9072
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-07-01 09:45:46 -0300

    Adicionando procura por nome  e numero

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@ using namespace std;
 
 //variaveis globais
     const int maxli=100, maxcol=2;
-    int op, linha=0, cont=0;
+    int op, linha=0, cont=0,quanticontatos=0;
     string contato[maxli][maxcol];
-    string nome[50], telefone[50];
+    string nome, num;
     string ma2[maxli][maxcol];
 
 //declarando funcoes
 void cadastrar(string x[maxli][maxcol]);
 void listar();
-void procurarnome(string y[][2]);
+void procurarnome();
+void procurarnumero();
 void pegarvalores();
-//
+void excluiragenda();
+void quantidadecontatos();
+//......................
 
 
 int main (){
@@ -24,41 +27,65 @@ int main (){
     fstream arquivo;
     arquivo.open("Agenda2023.txt", ios::out | ios::app);
     if (arquivo.is_open()){
+        pegarvalores();
+
         do{
         cout << "|       -----MENU-----       |" << endl;
         cout << "|1 - Adicionar um contato    |" << endl;
         cout << "|2 - Listar todos os contatos|" <<endl;
-        cout << "|3 - Procurar por contato    |"<<endl;
-        cout << "|4 - Alterar contato         |"<<endl;
-        cout << "|5 - Quantidade de contatos  |"<<endl;
-        cout << "|6 - Excluir agenda          |"<<endl;
-        cout << "|7 - Sair                    |"<<endl;
+        cout << "|3 - Procurar por nome       |"<<endl;
+        cout << "|4 - Procurar por numero     |"<<endl;
+        cout << "|5 - Alterar contato         |"<<endl;
+        cout << "|6 - Quantidade de contatos  |"<<endl;
+        cout << "|7 - Excluir agenda          |"<<endl;
+        cout << "|8 - Sair                    |"<<endl;
         cout << "|      Digite sua opcao      |" <<endl;
         cin >> op;
-
-        pegarvalores();
+  
         //Switch case do menu
 
         switch(op){
             case 1:
-                    cadastrar(contato);     
-                
-                break;
-            case 2:
-                //listar();//
-                cout << endl;
-                   for (linha = 0; linha < cont; linha++){
-       
-           
-                        cout<<contato[linha][0] << contato[linha][1];
-                        cout << endl;
 
-                   }
-                 
-          
-                break;
+             cadastrar(contato);     
+            break;
+
+            case 2:
+
+            listar();   
+            break;
+
             case 3:
-            procurarnome(contato);
+
+            procurarnome();
+            break;
+
+            case 4:
+
+            procurarnumero();
+            break;
+
+            case 5:
+
+            procurarnome();
+            break;
+
+            case 6:
+
+            quantidadecontatos();
+            break;
+
+            case 7:
+
+            char resposta;
+            cout << "Certeza que deseja excluir a agenda? [S/N]"<<endl;
+            cin >> resposta;
+                if(resposta == 'S' || resposta == 's'){
+                excluiragenda();
+                cout << "Agenda foi apaga com sucesso!"<<endl;
+                }else{
+                    cout << "Agenda não será excluída."<<endl;
+                }
             break;
 
             case 8:
@@ -70,125 +97,150 @@ int main (){
                 break;
             }
 
-    
         }while (op!=8);
+
     }else{
         cout << "Não foi possivel abrir o arquivo"<<endl;
     }
-
     arquivo.close();
-
     return 0;
 }
 
 void cadastrar(string x[maxli][maxcol]){
 
-
-
     fstream arquivo;
     arquivo.open("Agenda2023.txt", ios::out | ios::app);
-    
 
         cout<< "Digite seu nome:"<<endl;
-        
     
         for (linha = 0; linha < 1; linha++){
        
-           
             cin>>x[cont][0];
             arquivo << x[cont][0]<<endl;
-            
             
             cout << "Digite seu telefone:"<<endl;
             cin>> x[cont][1];
             arquivo << x[cont][1]<<endl;
-        
         }
            cont++;
-      
-        
-        
     arquivo.close();
-
-    
 }
-
 
 //função de listar 
-void listar (){
 
-    string line;
-    fstream arquivo2;
-    arquivo2.open("Agenda2023.txt", ios:: in);
-    while (getline(arquivo2, line))
-    {
-        cout << line<< " ";
-        cout << endl;
-    }
-    arquivo2.close();
-}
-
-void pegarvalores (){
-    int cont2 = 1, cont3=0;
-    
+void listar (){ 
     string line;
     fstream arquivo3;
 
     arquivo3.open("Agenda2023.txt",ios::in);
-    while (!arquivo3.eof()){
-        getline(arquivo3, line);
-        cont2++;
-
-        if (cont2%2 == 0){
-            ma2[cont3][0]=line;
-
-        }else if(cont2%2 != 0){
-            ma2[cont3][1]=line;
-            cont3++;
-        }
-    }
+  
+    pegarvalores();
     
-    for (int i = 0; i <2; i++)
+    for (int i = 0; i <100; i++)
     {
-        cout << ma2[i][0]<< endl;
-        cout << ma2[i][1] << endl;
-    }
-    
+        cout << ma2[i][0]<<" ";
+        cout << ma2[i][1];
+        cout << endl;
+    }   
     arquivo3.close();
 }
 
+  
 
-//funcao de procurar
+//função que pega os valores do arquivo txt e joga em uma matriz
+void pegarvalores (){
+    int   cont2 = 1;
+    
+    string line;
+    fstream arquivo4;
 
-void procurarnome (string y[][maxcol]){
+    arquivo4.open("Agenda2023.txt",ios::in);
+    while (!arquivo4.eof()){
+        getline(arquivo4, line);
 
-     for (linha = 0; linha < 1; linha++){    
-            
-            cout << "Digite o nome do contato que deseja procurar:"<<endl;
-            cin>> y[cont][0];
-            }
+        if (cont2%2 == 0){
+            ma2[cont2][1]=line;
 
-            bool veri = false;
-            pegarvalores();
-            for (int i = 0; i <100; i++)
-            {
-                if (y[cont][maxcol] == ma2[i][maxcol]){
-                    veri =  true;
-                
-                }else{
-                    veri = false;
-                }
-            }
-            
-            if(veri == true){
-                cout << "Contato existente";
-            } else if (veri == false){
-                cout << "Contato não encontrado";
-            }
-           cont++;
-
+        }else{
+            ma2[cont2][0]=line;
+          
+        }
+        cont2++;     
+    } 
+    arquivo4.close();
 }
 
 
+//funcao de procurar por nome
 
+void procurarnome (){
+        cout << "Digite o nome do contato que deseja procurar:"<<endl;
+        cin>> nome;
+    pegarvalores();
+     int procura=1, reprocura=0;
+            for (int i = 0; i <100; i++)
+            {
+                if (nome == ma2[i][0]){
+                    procura=0;
+                    cout << "Contato existente"<<endl;
+                    cout << ma2[i][0]<<endl;
+                
+                }else{
+                    reprocura =1;
+                }
+            } 
+        
+             if (reprocura == 1 && procura ==1 ){
+                cout << "Contato não encontrado" <<endl;
+            }
+}
 
+//funcao procurar por numero
+
+void procurarnumero(){
+        cout << "Digite o número do contato que deseja procurar:"<<endl;
+        cin>> num;
+    pegarvalores();
+     int procura=1, reprocura=0;
+            for (int i = 0; i <100; i++)
+            {
+                if (num == ma2[i][1]){
+                    procura=0;
+                    cout << "Número existente"<<endl;
+                    cout << ma2[i][1]<<endl;
+                
+                }else{
+                    reprocura =1;
+                }
+            } 
+        
+             if (reprocura == 1 && procura ==1 ){
+                cout << "Número não encontrado" <<endl;
+            }
+}
+//função para escluir a agenda
+void excluiragenda(){
+
+    remove("Agenda2023.txt");
+
+}
+
+//quantidade de cadastros
+
+void quantidadecontatos (){
+ string line;
+    fstream arquivo6;
+
+    arquivo6.open("Agenda2023.txt",ios::in);
+    while (!arquivo6.eof()){
+        getline(arquivo6, line);
+
+       quanticontatos++;
+    
+
+}
+
+cout << "A quantidade atual de contatos na agenda é de: "<< quanticontatos/2<<endl;
+    arquivo6.close();
+
+}
